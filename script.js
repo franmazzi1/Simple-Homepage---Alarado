65d12a9f91996fcb13ed58c28dcfa77aae4c2ddc
Francisco Mazzi <franciscomazzi110@gmail.com>
2025-01-29 13:55:39 -0300

    dark mode complete

--- script.js
+++ script.js
@@ -2,13 +2,7 @@ const toggleButton = document.getElementById('toggle-mode');
 const body = document.body;
 
 toggleButton.addEventListener('click', () => {
-
+    
     body.classList.toggle('dark-mode');
 
-
-    const moon = document.getElementById('moon');
-    const sun = document.getElementById('sun');
-
-
-    
 });
